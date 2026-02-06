7ac23606e3a3871951a63abbdb7bffc259ccc6a2
Nabot <nabot123@gmail.com>
2026-02-06 12:03:04 +0200

    Update login page to use company logo image

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,9 +16,9 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            <div class="mb-6">
+                <a href="/" class="flex justify-center">
+                    <img src="{{ asset('images/logo.jpg') }}" alt="{{ config('app.name', 'Laravel') }}" class="h-24 w-auto max-w-xs">
                 </a>
             </div>
 
